cab0b3c42662137e17d1ad9700cb3886fb6a03dc
maniksaluja <69381906+maniksaluja@users.noreply.github.com>
2024-09-20 17:20:55 +0530

    Update config.py

--- config.py
+++ config.py
@@ -13,7 +13,7 @@ class Config:
     API_HASH = os.environ.get("API_HASH", "9af2b4ce5315e0222d0b30eba7b0905d")
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "7488845217:AAHGbJtg4GJ3a4wIDF0Z9a4nJmjT-JRR440") 
     BOT_SESSION = os.environ.get("BOT_SESSION", "forward-bot") 
-    DB_URL = os.environ.get("DB_URL", "")
+    DB_URL = os.environ.get("DB_URL", "mongodb+srv://Manik:Manik11@cluster0.xtzuh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
     DB_NAME = os.environ.get("DB_NAME", "madflixbotz")
     OWNER_ID = [int(id) for id in os.environ.get("OWNER_ID", '6604279354').split()]
 
